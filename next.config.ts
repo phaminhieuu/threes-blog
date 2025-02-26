cd5f055c145d05b9c2f42e1cd6160a0aeeb9506c
phaminhieuu <minhhieupham851999@gmail.com>
2025-02-27 00:42:52 +0700

    use avif

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export"
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
